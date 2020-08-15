f6d1aec2ddd123619c182d98770eeb86fe60fbb9
Mubarak Ayelotan <12859241+mcmubby@users.noreply.github.com>
2020-08-15 20:21:36 +0100

    formatted written text

---
+++ src/Test file.md
@@ -0,0 +1,8 @@
+Project: Test file has been created
+Installation Instruction:
+ --This is the instruction section
+Usage:
+ --This is the usage section
+Contributor(s):
+ --This is the contributors section
+Created at: Sat Aug 15 2020 20:20:20 GMT+0100 (West Africa Standard Time)
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 //global variables
 let vb =1;
 const greetings = ["Enter Installation instruction: ", "Describe usage: ", "Contributors(use ',' to seperate): "];
+const heading = ["Installation Instruction", "Usage", "Contributor(s)"];
 
 //Create project file
 rl.question("Project name? : ", (res)=>{
@@ -19,7 +20,7 @@ rl.question("Project name? : ", (res)=>{
 
 testQ = (name,vb)=>{  
     rl.question(`${greetings[vb-1]}`, (inst)=>{
-        fs.appendFile(`${name}.md`,`\n ${inst}`, 
+        fs.appendFile(`${name}.md`,`\n${heading[vb-1]}:\n --${inst}`, 
         (err)=>{
             if(err){
                 console.log(err);
@@ -27,7 +28,15 @@ testQ = (name,vb)=>{
         });
         vb++;
         if(vb<4){testQ(name,vb);}
-        else{rl.close();}
+        else{
+            fs.appendFile(`${name}.md`,`\nCreated at: ${Date()}`, 
+                (err)=>{
+                    if(err){
+                        console.log(err);
+                    }
+            });
+            rl.close();
+        }
     });  
 }
 
